b70fedd97348a82f0af81e63b833b49b95d5441f
salem chirau <salemsc7@gmail.com>
2026-09-18 11:03:18 +0200

    Complete redesign of footer

--- src/components/FooterComponent/FooterComponent.jsx
+++ src/components/FooterComponent/FooterComponent.jsx
@@ -1,25 +1,52 @@
-import { Flex, Center, Image } from "@chakra-ui/react";
-import Logo from "../../assets/Logo.png";
+import { Link } from "react-router-dom";
+import { FaInstagram, FaFacebookF, FaPinterestP } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import css from "./FooterComponent.module.css";
 
 export default function FooterComponent() {
   return (
-    <Center
-      marginTop={{ base: "40px", md: "100px" }}
-      width={{ base: "100%", md: "100%" }}
-      height={{ base: "200px", md: "200px" }}
-      position="relative"
-      bottom="0"
-      left="0"
-      minH={"200px"}
-      bg={"black"}
-    >
-      <Image
-        src={Logo}
-        bg={"black"}
-        width={{ base: "50%", md: "90%" }}
-        maxW={{ base: "220px", md: "150px" }}
-        minW={"150px"}
-      />
-    </Center>
+    <footer className={css.footer}>
+      <div className={css.stars} aria-hidden="true" />
+      <div className={css.glow} aria-hidden="true" />
+      <div className={css.inner}>
+        <div className={css.brand}>
+          <p className={css.wordmark}>XENON</p>
+          <p className={css.tagline}>
+            Boutique pieces that catch the light after dusk.
+          </p>
+          <div className={css.social}>
+            <a href="#" aria-label="Instagram" className={css.socialLink}>
+              <FaInstagram />
+            </a>
+            <a href="#" aria-label="X" className={css.socialLink}>
+              <FaXTwitter />
+            </a>
+            <a href="#" aria-label="Facebook" className={css.socialLink}>
+              <FaFacebookF />
+            </a>
+            <a href="#" aria-label="Pinterest" className={css.socialLink}>
+              <FaPinterestP />
+            </a>
+          </div>
+        </div>
+
+        <nav className={css.columns} aria-label="Footer">
+          <div className={css.column}>
+            <h2 className={css.heading}>Shop</h2>
+            <Link to="/">Home</Link>
+            <Link to="/Checkout">Cart</Link>
+            <Link to="/login_signup">Account</Link>
+          </div>
+          <div className={css.column}>
+            <h2 className={css.heading}>Help</h2>
+            <a href="#">Shipping</a>
+            <a href="#">Returns</a>
+            <a href="#">Contact</a>
+          </div>
+        </nav>
+
+        <p className={css.copy}>© 2026 Xenon Boutique. All rights reserved.</p>
+      </div>
+    </footer>
   );
 }
